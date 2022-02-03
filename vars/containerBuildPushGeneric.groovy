7fcb7dcde010cfda170035e7d98e75d64b0141d5
Kurt Madel <kmadel@cloudbees.com>
2022-02-03 17:12:39 -0500

    Update containerBuildPushGeneric.groovy

--- vars/containerBuildPushGeneric.groovy
+++ vars/containerBuildPushGeneric.groovy
@@ -1,11 +1,10 @@
 // vars/containerBuildPushGeneric.groovy
-def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject = "core-workshop", Closure body) {
-  def dockerReg = "gcr.io/${gcpProject}"
-  def label = "kaniko-${repoOwner}"
+def call(String imageName, String imageTag = env.BUILD_NUMBER, String containerRegistry = "us-east1-docker.pkg.dev/core-workshop/workshop-registry", Closure body) {
+  def label = "kaniko-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/kaniko.yml'
   def customBuildArg = ""
   def buildModeArg = ""
-  podTemplate(name: 'kaniko', inheritFrom: 'default-jnlp', label: label, yaml: podYaml, podRetention: always(), idleMinutes: 30) {
+  podTemplate(name: 'kaniko', inheritFrom: 'default-jnlp', label: label, yaml: podYaml, podRetention: never(), activeDeadlineSeconds:1) {
     node(label) {
       body()
       try {
@@ -24,7 +23,8 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       container(name: 'kaniko', shell: '/busybox/sh') {
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
           sh label: "container build and push", script: """#!/busybox/sh
-            /kaniko/executor -f ${pwd()}/Dockerfile -c ${pwd()} ${buildModeArg} ${customBuildArg} --build-arg buildNumber=${BUILD_NUMBER} --build-arg commitAuthor='${COMMIT_AUTHOR}' --build-arg shortCommit=${env.SHORT_COMMIT} --cache=true -d ${dockerReg}/${imageName}:${imageTag}
+            /kaniko/docker-credential-gcr configure-docker --registries=${containerRegistry}
+            /kaniko/executor -f ${pwd()}/Dockerfile -c ${pwd()} ${buildModeArg} ${customBuildArg} --build-arg buildNumber=${BUILD_NUMBER} --build-arg commitAuthor='${COMMIT_AUTHOR}' --build-arg shortCommit=${env.SHORT_COMMIT} --cache=true -d ${containerRegistry}/${imageName}:${env.SHORT_COMMIT}
           """
         }
       }

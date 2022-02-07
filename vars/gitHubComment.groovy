cbb8f95b748041c6e9a2c4d031fa8b9f94d83603
Kurt Madel <kmadel@mac.com>
2022-02-07 15:34:56 -0500

    Update gitHubComment.groovy

--- vars/gitHubComment.groovy
+++ vars/gitHubComment.groovy
@@ -1,13 +1,17 @@
 def call(Map config) {
   echo "gitHubComment"
-  withCredentials([usernamePassword(credentialsId: "${config.credId}", usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
-    def commentId = sh(script: """
-      curl -s -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
-        -X POST -d '{"body": "${config.message}"}' \
-        "https://api.github.com/repos/${config.repoOwner}/${config.repo}/issues/${config.issueId}/comments" \
+  gitHubCredentialsId = config.credId ?: GITHUB_CREDENTIAL_ID
+  gitHubRepoOwner = config.repoOwner ?: REPO_OWNER
+  gitHubRepo = config.repo ?: REPO_NAME
+  env.MESSAGE="${config.message}"
+  withCredentials([usernamePassword(credentialsId: gitHubCredentialsId, usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
+    def commentId = sh(script: '''
+      curl -s -H "authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+        -X POST -d  '{"body":"'"${MESSAGE}"'"}' \
+        "https://api.github.com/repos/$gitHubRepoOwner/$gitHubRepo/issues/$CHANGE_ID/comments" \
       | jq -r '.id' | tr -d '\n' 
-    """, returnStdout: true)
+    ''', returnStdout: true)
     echo "gitHubComment commentId: ${commentId}"
     return commentId
   }
-}        
+}      

47127b5738e712b4b985cf90639aa54dba0368ef
Kurt Madel <kmadel@mac.com>
2022-02-07 12:37:31 -0500

    Update containerBuildPushGeneric.groovy

--- vars/containerBuildPushGeneric.groovy
+++ vars/containerBuildPushGeneric.groovy
@@ -1,12 +1,14 @@
 // vars/containerBuildPushGeneric.groovy
-def call(String imageName, String imageTag, String containerRegistry = "us-east1-docker.pkg.dev/core-workshop/workshop-registry", Closure body) {
+def call(String imageName, 
+         String containerRegistry = "us-east1-docker.pkg.dev/core-workshop/workshop-registry", 
+         Closure body) {
   def label = "kaniko-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/kaniko.yml'
   def customBuildArg = ""
   podTemplate(name: 'kaniko', inheritFrom: 'default-jnlp', label: label, yaml: podYaml, podRetention: never(), activeDeadlineSeconds:1) {
     node(label) {
       body()
-      def tag = imageTag ?: env.SHORT_COMMIT
+      def tag = env.IMAGE_TAG ?: env.SHORT_COMMIT
       if(env.EVENT_BASE_IMAGE_TAG) {
         customBuildArg = "--build-arg BASE_IMAGE_TAG=${env.EVENT_BASE_IMAGE_TAG}"
       }
@@ -15,7 +17,7 @@ def call(String imageName, String imageTag, String containerRegistry = "us-east1
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
           sh label: "kaniko build and push container image", script: """#!/busybox/sh
             /kaniko/docker-credential-gcr configure-docker --registries=${containerRegistry}
-            /kaniko/executor -f ${pwd()}/Dockerfile -c ${pwd()} ${customBuildArg} --build-arg commitAuthor='${COMMIT_AUTHOR}' --build-arg shortCommit=${env.SHORT_COMMIT} --cache=true -d ${containerRegistry}/${imageName}:${tag}
+            /kaniko/executor -f ${pwd()}/Dockerfile -c ${pwd()} ${customBuildArg} --build-arg COMMIT_AUTHOR='${COMMIT_AUTHOR}' --build-arg GIT_COMMIT=${env.COMMIT_SHA} --cache=true -d ${containerRegistry}/${imageName}:${tag}
           """
         }
       }
